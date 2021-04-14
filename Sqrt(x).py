97d472d52f8b023b47d8f4e1e2e2793f938d1b57
Bavan M Y <70379421+bavan172@users.noreply.github.com>
2021-04-14 09:35:56 +0530

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,9 @@
+# Given a non-negative integer x, compute and return the square root of x.
+# Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+    
+    
+class Solution:
+   def mySqrt(self, x: int) -> int:
+       x = int(sqrt(x))
+       return x
+    
